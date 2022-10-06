469d85610bf752918082bb8ab11259844de72e95
Kristina-VCyber <hatkova@inbox.ru>
2022-10-06 20:47:50 +0300

    12_addItemForm_story

---
+++ src/AddItemForm.stories.tsx
@@ -0,0 +1,12 @@
+import { AddItemForm } from "./AddItemForm";
+import { action } from "@storybook/addon-actions";
+import React from "react";
+
+export default {
+    title: "AddItemForm Component",
+    component: AddItemForm
+}
+const callback = action("Button 'add' was pressed inside the form");
+export const AddItemFormExample = (props:any) => {
+    return <AddItemForm addItem={callback}/>
+}
